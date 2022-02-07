53f7a28b41f73b4a4298f9030b452a55fc0a05d0
hilmizul <netspytux@gmail.com>
2022-02-07 12:35:22 +0700

    hapus link about

--- src/components/Navigasi.vue
+++ src/components/Navigasi.vue
@@ -3,7 +3,6 @@
     <nav class="nav">
       <router-link to="/" class="nav-link">home</router-link> 
       <router-link to="/products" class="nav-link">products</router-link> 
-      <router-link to="/about" class="nav-link">about</router-link> 
       <router-link to="/contact" class="nav-link">contact</router-link> 
     </nav>
   </div>
